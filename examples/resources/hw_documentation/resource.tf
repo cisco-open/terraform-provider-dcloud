0d1a9d83ce00ca453effa3f8e079c3fe814a2193
Mohit Hooda <mohit@Mohits-MacBook-Air.local>
2023-09-28 12:33:18 +0530

    Added example files in hw_documentation

---
+++ examples/resources/hw_documentation/resource.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    dcloud = {
+      version = "0.1"
+      source  = "cisco.com/cisco-open/dcloud"
+    }
+  }
+}
+
+provider "dcloud" {
+  tb_url = "https://tbv3-production.ciscodcloud.com/api"
+}
+
+resource "dcloud_topology" "test_topology" {
+  name        = "My Terraformed Topology"
+  description = "A topology created from terraform"
+  notes       = "Programmatic clients rule!"
+  datacenter  = "LON"
+}
+
+resource "dcloud_documentation" "documentation" {
+  topology_uid = dcloud_topology.test_topology.id
+  doc_url = "https://johndoe.com"
+}
